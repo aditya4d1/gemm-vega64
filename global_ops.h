68a1ee7e666862576a72f81ad8048e86be01ccfd
Aditya Atluri <adityaatluri@users.noreply.github.com>
2017-11-08 00:32:48 -0600

    Update global_ops.h

--- global_ops.h
+++ global_ops.h
@@ -7,3 +7,11 @@ asm volatile("\n \
   :"=v"(val)
   :"v"(ptr));
 }
+
+inline __device__ void global_load_16(float4* &ptr, float4 &val, uint32_t offset) {
+asm volatile("\n \
+  global_load_dwordx4 %0, %1, off offset:256\n \
+  "
+  :"=v"(val)
+  :"v"(ptr));
+}
