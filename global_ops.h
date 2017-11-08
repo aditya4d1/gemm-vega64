973aafef85134eef89ae6c79c5c0e2162c430f7b
Aditya Atluri <adityaatluri@users.noreply.github.com>
2017-11-08 00:10:47 -0600

    Update global_ops.h

--- global_ops.h
+++ global_ops.h
@@ -1,6 +1,6 @@
 #pragma once
 
-inline __device__ void global_blocks(float4* &ptr, float4 &val, uint32_t offset) {
+inline __device__ void global_load(float4* &ptr, float4 &val, uint32_t offset) {
 asm volatile("\n \
   global_load_dwordx4 %0, %1, off \n \
   "
